e80714d77f6e2ef5e8216591aa372986acba6052
Your Name <nrahnemoon@gmail.com>
2016-11-27 12:10:52 -0500

    added IP address for connection

--- app/src/main/java/edu/cmu/melle/base/mellebase/MainActivity.java
+++ app/src/main/java/edu/cmu/melle/base/mellebase/MainActivity.java
@@ -5,24 +5,55 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.GpsSatellite;
+import android.location.GpsStatus;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.PowerManager;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Iterator;
 
 public class MainActivity extends AppCompatActivity implements LocationListener, SensorEventListener {
 
     private SensorManager mSensorManager;
     SensorEventListener mSensorListener;
 
+    double mLatitude;
+    double mLongitude;
+    double mAzimuth;
+    double mRoll;
+    double mPitch;
+    int mNumSatellites;
+
+    LocationManager mLocationManager;
+    long mLastLocationMillis;
+    Location mLastLocation;
+    boolean mIsGPSFix;
+
+    ROSBridge mRosBridge;
+    String mIpAddress;
+    String mPortNumber;
+
     TextView mLatitudeText;
     TextView mLongitudeText;
     TextView mRollText;
     TextView mPitchText;
     TextView mAzimuthText;
-
+    TextView mNumSatellitesText;
+    EditText mIpAddressText;
     LowPassFilter filterYaw = new LowPassFilter(0.03f);
     LowPassFilter filterPitch = new LowPassFilter(0.03f);
     LowPassFilter filterRoll = new LowPassFilter(0.03f);
@@ -39,6 +70,12 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Wake lock
+        PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                "MyWakelockTag");
+        wakeLock.acquire();
+
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
 
         mLatitudeText = (TextView) findViewById(R.id.latitudeText);
@@ -46,6 +83,15 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         mRollText = (TextView) findViewById(R.id.rollText);
         mPitchText = (TextView) findViewById(R.id.pitchText);
         mAzimuthText = (TextView) findViewById(R.id.azimuthText);
+        mNumSatellitesText = (TextView) findViewById(R.id.numSatellitesText);
+        mIpAddressText = (EditText) findViewById(R.id.ipAddressText);
+
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        mIpAddress = "192.168.43.225";
+        mPortNumber = "9090";
+        mRosBridge = new ROSBridge(mIpAddress, mPortNumber);
+        mRosBridge.start();
     }
 
     @Override
@@ -55,17 +101,33 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_NORMAL);
         mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),SensorManager.SENSOR_DELAY_NORMAL);
 
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
     }
 
     // The following method is required by the SensorEventListener interface;
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
 
+    private void publishToRos() {
+        JSONObject json = new JSONObject();
+        try {
+            json.put("latitude", mLatitude);
+            json.put("longitude", mLongitude);
+            json.put("azimuth", mAzimuth);
+            json.put("roll", mRoll);
+            json.put("pitch", mPitch);
+            json.put("sats", mNumSatellites);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        mRosBridge.publishToTopic("/AndroidSensorData", json);
+    }
+
     // The following method is required by the SensorEventListener interface;
     // Hook this event to process updates;
     public void onSensorChanged(SensorEvent event) {
+
 
         switch (event.sensor.getType())
         {
@@ -95,19 +157,48 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
             float pitch = (float) Math.toDegrees(values[1]);
             float roll = (float) Math.toDegrees(values[2]);
 
-            mAzimuthText.setText(Double.toString(Math.toDegrees(values[0])));
-            mRollText.setText(Float.toString(filterRoll.lowPass(roll)));
-            mPitchText.setText(Float.toString(filterPitch.lowPass(pitch)));
+            mAzimuth = Math.toDegrees(values[0]);
+            if(mAzimuth<0)
+                mAzimuth+=360;
 
+            mPitch = Math.toDegrees(values[1]);
+            mRoll = Math.toDegrees(values[2]);
 
+            mAzimuthText.setText(Double.toString(mAzimuth));
+            mRollText.setText(Double.toString(mRoll));
+            mPitchText.setText(Double.toString(mPitch));
 
+            publishToRos();
+        }
+
+        if ((SystemClock.elapsedRealtime() - mLastLocationMillis) > 3000) {
+            mNumSatellites = 0;
         }
     }
 
     @Override
     public void onLocationChanged(Location loc) {
-        mLatitudeText.setText("" + loc.getLatitude());
-        mLongitudeText.setText("" + loc.getLongitude());
+
+        if (loc == null) {
+            mNumSatellites = 0;
+            return;
+        }
+
+        mLastLocationMillis = loc.getTime();
+        if ((SystemClock.elapsedRealtime() - mLastLocationMillis) < 3000) {
+            mNumSatellites = 1;
+        } else {
+            mNumSatellites = 0;
+        }
+
+        mLatitude = loc.getLatitude();
+        mLongitude = loc.getLongitude();
+
+        mLatitudeText.setText("" + mLatitude);
+        mLongitudeText.setText("" + mLongitude);
+        mNumSatellitesText.setText("" + mNumSatellites);
+
+        publishToRos();
     }
 
     @Override
@@ -121,4 +212,16 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
     }
+
+    public void connect(View view) {
+        if(mIpAddressText.getText().toString().equals("")){
+            Toast.makeText(getApplicationContext(),"Please input a IP Address",Toast.LENGTH_SHORT).show();
+           return;
+        }
+        mIpAddress=mIpAddressText.getText().toString();
+        mPortNumber="9090";
+        mRosBridge = new ROSBridge(mIpAddress, mPortNumber);
+        mRosBridge.start();
+    }
+
 }
